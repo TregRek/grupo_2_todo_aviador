05a651dc75cf621d15ae40f66f2af3de346fed48
Augusto <asgarcia103@gmail.com>
2021-02-15 20:32:48 -0500

    Se armó la pagina de edicion de producto

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const path = require('path');
 
 var indexRouter = require('./routes/index');
 var productRouter = require('./routes/producto');
-// const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, './public');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicPath));
 
 app.listen(3000, console.log("Server's up! Port 3000"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
-
 app.use('/producto', productRouter);
