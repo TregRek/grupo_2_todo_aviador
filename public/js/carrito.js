00ef149ca4b853ef19b74a5360f5066082255d3a
Luuchoh <luxo997@gmail.com>
2021-06-03 18:30:06 -0500

    Arreglando bugs precio carrito

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -6,19 +6,19 @@ window.addEventListener("load", ()=>{
     let subt = document.querySelector('span #subtotal');
     let price = document.querySelector('span #precio')
 
-    subt.textContent = (parseInt(price.textContent) * parseInt(num_prod.value));
+    subt.textContent = parseFloat(parseFloat(price.textContent) * parseFloat(num_prod.value)).toFixed(2);
 
     mas.addEventListener('click',(e)=>{
         e.preventDefault();
         num_prod.value = (parseInt(num_prod.value) + 1);
-        subt.textContent = (parseInt(subt.textContent) + parseInt(price.textContent));
+        subt.textContent = parseFloat(parseFloat(subt.textContent) + parseFloat(price.textContent)).toFixed(2);
     })
 
     menos.addEventListener('click',(e)=>{
         e.preventDefault();
         if(num_prod.value > 1){
         num_prod.value -= 1;
-        subt.textContent = (parseInt(subt.textContent) - parseInt(price.textContent));
+        subt.textContent = parseFloat(parseFloat(subt.textContent) - parseFloat(price.textContent)).toFixed(2);
         }
     })
 
